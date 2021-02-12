187fb1d43551294f8f55d25c0462e7785b741522
Nadelvorhang <skubiduja1981@gmail.com>
2021-02-12 14:04:40 +0100

    bot base code

---
+++ index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const config = require('./config.json');
+
+client.on("ready", () => {
+    console.log("Logged into "+client.user.username)
+    client.user.setActivity("mit Members", { type: 'PLAYING' });
+})
+
+client.login(config.token);
