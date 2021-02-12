a6560e1f02cd85359897c78e9d3b53ec91b7ae53
Nadelvorhang <skubiduja1981@gmail.com>
2021-02-12 14:25:50 +0100

    Update

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ client.on("ready", () => {
     console.log("Logged into "+client.user.username)
     client.user.setActivity("mit Members", { type: 'PLAYING' });
 })
-
+client.on("message", message => {
+    if(message.content != "test") return;
+    message.channel.send("Im working.")
+})
 client.login(process.env.TOKEN);
 // hello hve
