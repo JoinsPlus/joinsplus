6d3adc30e4ce7ddc8895ad8414d469bfaae2ba3c
Nadelvorhang <skubiduja1981@gmail.com>
2021-02-13 21:50:23 +0100

    Update

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ client.on('message', async (msg) => {
             })
             await user.save()
         }
-        msg.reply(new Discord.MessageEmbed().setTitle("Profile Information").setAuthor(msg.author.username, msg.author.displayAvatarURL).addField("Coins", user.coins, true))
+        msg.reply(new Discord.MessageEmbed().setTitle("Profile Information").setAuthor(msg.author.username, msg.author.displayAvatarURL()).addField("Coins", user.coins, true))
     }
 })
 
