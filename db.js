c623c25c012feefc37c43153bba690ec734433a6
Nadelvorhang <skubiduja1981@gmail.com>
2021-02-13 21:42:27 +0100

    Update

---
+++ db.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose')
+mongoose.connect(`mongodb+srv://${process.env.DBUSER}:${process.env.DBPASS}@cluster0.pia0b.mongodb.net/joinsplus?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+module.exports = {
+    User: mongoose.model('user', {
+        _id: { /* Discord ID */
+            type: String,
+            required: true
+        },
+        coins: { /* The coins the user has */
+            type: Number,
+            default: 0
+        },
+        /* idk what parameters u want here */
+    }),
+    Guild: mongoose.model('guild', {
+        _id: {
+            type: String,
+            required: true
+        },
+        coins: { /* The Number of Members that need to join the Server after they bought Ads */
+            type: Number,
+            default: 0
+        }
+    }),
+    Reward: mongoose.model('reward', { /* An instance of this is created when a user joins using our invite, and gets reversed if the user leaves. */
+        owner: { // The ID of the user
+            type: String,
+            required: true
+        },
+        guild: {
+            type: String,
+            required: true
+        },
+        awards: {
+            type: Number,
+            required: true
+        },
+        expires: {
+            type: Number,
+            required: true
+        }
+    })
+}
--- index.js
+++ index.js
@@ -1,10 +1,27 @@
 const Discord = require('discord.js')
 const client = new Discord.Client()
+const db = require('./db')
 
 client.on("ready", () => {
     console.log("Logged into "+client.user.username)
     client.user.setActivity("mit Members", { type: 'PLAYING' });
 })
+
+client.on('message', async (msg) => {
+    if (msg.content.toLocaleLowerCase() == "profile") {
+        let user = await db.User.findOne({
+            _id: msg.author.id
+        })
+        if (!user) {
+            user = new db.User({
+                _id: msg.author.id
+            })
+            await user.save()
+        }
+        msg.reply(new Discord.MessageEmbed().setTitle("Profile Information").setAuthor(user).addField("Coins", user.coins, true))
+    }
+})
+
 client.on("message", message => {
     if(message.content != "test") return;
     message.channel.send("Im working.")
