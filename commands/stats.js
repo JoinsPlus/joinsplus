1d38c6e9c2444ab837e2b7e2c051660469e8cc42
Nadelvorhang <skubiduja1981@gmail.com>
2021-03-03 15:25:05 +0100

    Update

--- commands/stats.js
+++ commands/stats.js
@@ -26,10 +26,10 @@ module.exports = {
     .setTitle("Checking...").setColor(9807270);
         message.channel.send(checking).then((msg) => {
             const statsinfo = new Discord.MessageEmbed()
-            .setTitle("Bot statistics")
+            .setTitle("↔ Bot statistics ↔")
             .setAuthor(message.author.username, message.author.displayAvatarURL())
             .setThumbnail(client.user.displayAvatarURL())
-            .setDescription(`❗ **Library:** \`Discord.js - V12\`\n❕ **Shard Ram Usage:** \`${Math.round((process.memoryUsage().heapUsed / 1048576))} / ${Math.round((parseInt(process.env.MAXMEM) || os.totalmem()) / 1048576)}MiB\`\n❗ **Total Ram Usage** \`${Math.round(((os.totalmem() - os.freemem()) / 1048576))}MiB/${Math.round(os.totalmem() / 1048576)}MiB\`\n❕ **CPU Usage:** \`${os.loadavg()[0].toString()}%\`\n❗ **Uptime:** \`${msToTime(client.uptime)}\`\n❕ **Servers:** \`${client.guilds.cache.size}\`\n❗ **Users:** \`${client.guilds.cache.map(s => s.memberCount).reduce((a, b) => a + b)}\`\n❕ **Channels:** \`${client.channels.cache.size}\`\n\n[(click here for support)](${process.env.SUPPORT_LINK}) [(click here to invite the bot)](${process.env.INVITE_LINK})`)
+            .setDescription(`❗ **Library:** \`Discord.js - V12\`\n❕ **Shard Ram Usage:** \`${Math.round((process.memoryUsage().heapUsed / 1048576))}MiB / ${Math.round((parseInt(process.env.MAXMEM) || os.totalmem()) / 1048576)}MiB\`\n❗ **Total Ram Usage** \`${Math.round(((os.totalmem() - os.freemem()) / 1048576))}MiB/${Math.round(os.totalmem() / 1048576)}MiB\`\n❕ **CPU Usage:** \`${os.loadavg()[0].toString()}%\`\n❗ **Uptime:** \`${msToTime(client.uptime)}\`\n❕ **Servers:** \`${client.guilds.cache.size}\`\n❗ **Users:** \`${client.guilds.cache.map(s => s.memberCount).reduce((a, b) => a + b)}\`\n❕ **Channels:** \`${client.channels.cache.size}\`\n\n[join support](${process.env.SUPPORT_LINK}) **|** [bot invite](${process.env.INVITE_LINK})`)
             .setColor(9807270)
             .setTimestamp();
             const stopped = new Date();
