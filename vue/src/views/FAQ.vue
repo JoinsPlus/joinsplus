c90a2c3d42510f7468133951d01db7baa6fdc619
Nadelvorhang <skubiduja1981@gmail.com>
2021-03-03 20:24:00 +0100

    Update

--- vue/src/views/FAQ.vue
+++ vue/src/views/FAQ.vue
@@ -2,9 +2,11 @@
   <div>
     <h1>Frequently Aasked Questions</h1>
     <div class="wrapper">
-      <div v-for="category in categories" :key="category" class="category">
+      <div v-for="category in categories" :key="category._id" class="category">
         <p class="categoryTitle">{{category.title}}</p>
-        <div>
+        <div v-for="faq in faqs.filter((a) => {return a.parent == category._id})" :key="faq._id">
+          <p>{{faq.title}}</p>
+          <vue-markdown>{{faq.content}}</vue-markdown>
         </div>
       </div>
     </div>
@@ -12,20 +14,29 @@
 </template>
 
 <script>
+import VueMarkdown from 'vue-markdown'
+
 // @ is an alias to /src
 export default {
   name: "FAQ",
-  components: {},
+  components: {
+    VueMarkdown
+  },
   data() {
     return {
       categories: [
         {
-          id: 'loading',
+          _id: 'loading',
           title: 'Loading FAQ...'
         }
       ],
       faqs: [
-
+        {
+          _id: 'loadingf',
+          parent: 'loading',
+          title: 'Is this loading?',
+          content: '~~Yes~~ **No**, not really. This part isn\'t implemented yet :)[https://joins.plus/freecoin](https://www.youtube.com/watch?v=dQw4w9WgXcQ)'
+        }
       ]
     };
   },
